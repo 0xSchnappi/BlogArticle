68f687225ebe8d53f6f9483b80b596a9122e2e6b
0xSchnappi <952768182@qq.com>
2023-08-17 22:06:34 +0800

    初始化ghidra

---
+++ ghidra/example/ch2_cpp_example.cpp
@@ -0,0 +1,28 @@
+void demo(void)
+{
+
+}
+void demo(int x)
+{
+
+}
+void demo(double x)
+{
+
+}
+void demo(int x, double y)
+{
+
+}
+void demo(double x, int y)
+{
+
+}
+void demo(char* str)
+{
+
+}
+int main()
+{
+    return 0;
+}
